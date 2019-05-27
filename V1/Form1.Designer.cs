1d367088744f160b41a06a2340382e8984b9a71a
Apple1501 <kunure15@gmail.com>
2019-05-27 23:38:42 +0300

    Добавьте файлы проекта.

---
+++ V1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxp = new System.Windows.Forms.TextBox();
+            this.textBoxc = new System.Windows.Forms.TextBox();
+            this.textBoxL = new System.Windows.Forms.TextBox();
+            this.textBoxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(175, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Исходные данные ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(28, 64);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "p(кг/м^3) =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(11, 102);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "c(Дж/кг*С) =";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(66, 144);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "L(м) =";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(74, 185);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "t(c) =";
+            // 
+            // textBoxp
+            // 
+            this.textBoxp.Location = new System.Drawing.Point(134, 60);
+            this.textBoxp.Name = "textBoxp";
+            this.textBoxp.Size = new System.Drawing.Size(251, 28);
+            this.textBoxp.TabIndex = 5;
+            this.textBoxp.TextChanged += new System.EventHandler(this.TextChanged);
+            this.textBoxp.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputKey);
+            this.textBoxp.MouseEnter += new System.EventHandler(this.MouseEnter);
+            // 
+            // textBoxc
+            // 
+            this.textBoxc.Location = new System.Drawing.Point(134, 102);
+            this.textBoxc.Name = "textBoxc";
+            this.textBoxc.Size = new System.Drawing.Size(250, 28);
+            this.textBoxc.TabIndex = 6;
+            // 
+            // textBoxL
+            // 
+            this.textBoxL.Location = new System.Drawing.Point(135, 139);
+            this.textBoxL.Name = "textBoxL";
+            this.textBoxL.Size = new System.Drawing.Size(248, 28);
+            this.textBoxL.TabIndex = 7;
+            // 
+            // textBoxt
+            // 
+            this.textBoxt.Location = new System.Drawing.Point(135, 182);
+            this.textBoxt.Name = "textBoxt";
+            this.textBoxt.Size = new System.Drawing.Size(249, 28);
+            this.textBoxt.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1242, 767);
+            this.Controls.Add(this.textBoxt);
+            this.Controls.Add(this.textBoxL);
+            this.Controls.Add(this.textBoxc);
+            this.Controls.Add(this.textBoxp);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Курсовая работа ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxp;
+        private System.Windows.Forms.TextBox textBoxc;
+        private System.Windows.Forms.TextBox textBoxL;
+        private System.Windows.Forms.TextBox textBoxt;
+    }
+}
+
---
+++ V1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace V1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Подпись текстбоксов на событие 
+            textBoxc.KeyPress += InputKey;
+            textBoxL.KeyPress += InputKey;
+            textBoxt.KeyPress += InputKey;
+            textBoxc.TextChanged += TextChanged;
+            textBoxL.TextChanged += TextChanged;
+            textBoxt.TextChanged += TextChanged;
+            textBoxc.MouseEnter += MouseEnter;
+            textBoxL.MouseEnter += MouseEnter;
+            textBoxt.MouseEnter += MouseEnter;
+
+        }
+
+        //ввод только цифр и запятой 
+         void InputKey(object sender, KeyPressEventArgs e)
+        {
+            //передача элемента управления 
+            TextBox tb = (TextBox)sender;
+            // проверка -ввод только цифр и Backspace
+            if (e.KeyChar >= '0' && e.KeyChar <= '9'|| (e.KeyChar == (char)8)) return;
+            //замена точки на запятую
+            if (e.KeyChar == '.') e.KeyChar = ',';
+            // проверка, что запятая только одна. И она не первый символ
+            if (e.KeyChar == ',')
+            {
+                if (tb.Text.Length == 0 || tb.Text.IndexOf(',') != -1)
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+            e.Handled = true;
+        }
+
+        new void TextChanged(object sender, EventArgs e)
+        { 
+            //передача элемента управления 
+            TextBox tb = (TextBox)sender;
+            tb.ShortcutsEnabled = false;
+           
+        }
+
+
+      new  void MouseEnter(object sender, EventArgs e)
+        {
+            //передача элемента управления 
+            TextBox tb = (TextBox)sender;
+            tb.ShortcutsEnabled = false;
+        }
+    }
+}
